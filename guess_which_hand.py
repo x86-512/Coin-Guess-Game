e1766782c816c274b891eaf2474b073aa9c34c17
x256_64 <70492320+x256-64@users.noreply.github.com>
2024-07-18 00:41:28 +0000

    Initial Upload

---
+++ guess_which_hand.py
@@ -0,0 +1,24 @@
+import qsharp
+
+#Created on 7/17/2024
+def main() -> None:
+    while True:
+
+        guess:str = input("Which hand do I have a coin in?\n> ")
+
+        if guess.lower()=="exit":
+            exit()
+
+        if guess.lower()!="right" and guess.lower()!="left":
+            print("Invalid input, it's either left or right, else type exit to quit")
+            continue
+
+        break
+
+    qsharp.init(project_root = "./")
+
+    computer_guess = "left" if str(qsharp.eval("quantum_disaster.randomBit()"))=="One" else "right"
+    print("\x1b[32mYou are correct\x1b[0m" if computer_guess == guess else "\x1b[31mYou are incorrect\x1b[0m")
+
+if __name__=="__main__":
+    main()
---
+++ src/guess_which_hand.qs
@@ -0,0 +1,16 @@
+﻿
+namespace quantum_disaster {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+ 
+	//Console input doesn't work with Q#, so I am using python instead
+    operation randomBit() : Result {
+		use q = Qubit(); 
+		H(q); //Uses the Hammard Gate transform to create a superposition state 
+		let returnable = M(q); //Sets the qubit to |0> or |1>
+		Reset(q); //Reminds me of free() or delete
+		return returnable;
+    }
+}
+
